a8a279488520822c6bc7252bc20ef47b745be3e0
Deviljhinn <95416268+Deviljhinn@users.noreply.github.com>
2022-06-27 20:35:24 -0400

    first commit

---
+++ Desktop/ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x79b2ffc30cdc621229837f9f08ed616a913f374f";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ Desktop/ICO/notes from quinn.md
@@ -0,0 +1,4 @@
+/* This is the contract address for the ICO token for CryptoDevs
+Anyone with a CryptoDev NFT can claim it*/
+
+0xb6581506582C8bAcF4983CbcCe24FF9e94fC98D7
